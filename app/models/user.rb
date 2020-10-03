868a3ab869689ebbab92581409b71331b8381430
tam03 <tamuramura2803@gmail.com>
2020-10-03 19:50:08 +0900

    to arrange view

---
+++ app/models/user.rb
@@ -0,0 +1,14 @@
+class User < ApplicationRecord
+
+  def book
+    return Book.where(user_id: self.id)
+  end
+
+  validates :name, presence: true
+  validates :profile, length: { maximum: 200 }
+
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+end
