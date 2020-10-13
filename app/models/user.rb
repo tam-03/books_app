86d701ae00d0297004e749dcb06cf8d760f08599
tam03 <tamuramura2803@gmail.com>
2020-10-13 12:15:40 +0900

    add user_model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   has_many :books, dependent: :destroy
-
+  has_one_attached :avatar
   validates :name, presence: true
   validates :profile, length: { maximum: 200 }
 
