2998d44252940f543eccfa5d4527378f8f3eaae9
tam03 <tamuramura2803@gmail.com>
2020-10-13 11:44:39 +0900

    set callback_controller

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -2,6 +2,16 @@
 
 module Users
   class OmniauthCallbacksController < Devise::OmniauthCallbacksController
+    def github
+      @user = User.from_omniauth(request.env['omniauth.auth'].except('extra'))
+
+      if @user.persisted?
+        sign_in_and_redirect @user
+      else
+        session['devise.user_attributes'] = @user.attributes
+        redirect_to new_user_registration_url
+      end
+    end
     # You should configure your model like this:
     # devise :omniauthable, omniauth_providers: [:twitter]
 
