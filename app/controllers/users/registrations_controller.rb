c440cc9d2a44c71ade6d8940515b7935efa423d8
tam03 <tamuramura2803@gmail.com>
2020-10-23 17:53:05 +0900

    change books/index view picture and picture size, delete unuse comment

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -2,7 +2,6 @@
 
 module Users
   class RegistrationsController < Devise::RegistrationsController
-    #protected
 
     # before_action :configure_sign_up_params, only: [:create]
     # before_action :configure_account_update_params, only: [:update]
