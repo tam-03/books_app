d0723b4212b5629483036e66d4552498121cdf1e
tam03 <tamuramura2803@gmail.com>
2020-10-27 18:44:29 +0900

    set routes 'user_relationships'

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  get 'users/show'
   resources :books
   root 'books#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
@@ -8,14 +7,13 @@ Rails.application.routes.draw do
     sessions: 'users/sessions',
     omniauth_callbacks: 'users/omniauth_callbacks'
   }
-  get 'users/show' => 'users#show'
   devise_scope :user do
     get 'sign_in', to: 'users/sessions#new'
     get 'sign_out', to: 'users/sessions#destroy'
   end
 
   resources :users do
-    resource :relationships, only: [:create, :destroy]
+    resource :user_relationships, only: [:create, :destroy]
     get :follows, on: :member
     get :followers, on: :member
   end
