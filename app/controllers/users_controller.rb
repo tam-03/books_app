2f2b506c4d72d44deea539f9f2ea71ef0dde1088
tam03 <tamuramura2803@gmail.com>
2020-10-27 19:42:41 +0900

    change syntax error

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,10 @@
 class UsersController < ApplicationController
-
   def index
     @users = User.all
   end
 
   def show
-    @user = user = User.find(params[:id])
+    @user = User.find(params[:id])
   end
 
   def follows
