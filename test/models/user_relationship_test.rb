26b1ebdbfee986c50c649e10961934f9fb38b60d
tam03 <tamuramura2803@gmail.com>
2020-11-22 15:00:08 +0900

    create model test

---
+++ test/models/user_relationship_test.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+require 'test_helper'
+
+class UserRelationshipTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,19 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "followed_by?" do
+    me = User.create!(id: 1, name: "me", email: "me@example.com", password: "password")
+    she = User.create!(id: 2, name: "she", email: "she@example.com", password: "password")
+
+    follow = me.active_relationships.build(follower_id: 2)
+    follow.save
+
+    assert_not me.followed_by?(she)
+    assert she.followed_by?(me)
+  end
+
+  test "set_dummy_email" do
+    auth_user =  User.create!(name: "auth_user", email: "auth_user@example.com", password: "password", uid: 1, provider: "github")
+    assert_equal User.dummy_email(auth_user), "1-github@example.com"
+  end
 end
